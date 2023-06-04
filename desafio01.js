5b8c7cdd2058924a92de464c5606f4bbf4d6901c
Erick Osuna Saavedra <erick_osuna@hotmail.com>
2023-06-03 21:22:17 -0300

    Primer Commit

---
+++ desafio01.js
@@ -0,0 +1,16 @@
+class ProductManager {
+
+    constructor(product){
+        this.product = product;
+        const contador = 0;
+        const contadorGlobal = 0;
+    }
+
+    getResponsable() {
+
+        
+
+    }
+
+
+}
